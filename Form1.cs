b4e9405e9b77bbe8e7ab0bab6562012c138ffff7
candylifter <candylifter@gmail.com>
2015-05-20 22:47:11 +0300

    Initial

---
+++ Form1.cs
@@ -0,0 +1,477 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Informatikos_projektas
+{
+    public partial class Form1 : Form
+    {
+        static char[] globalhand;
+        static char[] globalBoard = new char[7];
+        static int pts;
+        MainClass load = new MainClass();
+        List<string> wordList = new List<string>();
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                dispBoard(globalhand[0]);
+                pictureBox1.Image = null;
+            }
+        }
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            if (pictureBox2.Image != null)
+            {
+                dispBoard(globalhand[1]);
+                pictureBox2.Image = null;
+            }
+        }
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            if (pictureBox3.Image != null)
+            {
+                dispBoard(globalhand[2]);
+                pictureBox3.Image = null;
+            }
+        }
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            if (pictureBox4.Image != null)
+            {
+                dispBoard(globalhand[3]);
+                pictureBox4.Image = null;
+            } 
+        }
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            if (pictureBox5.Image != null)
+            {
+                dispBoard(globalhand[4]);
+                pictureBox5.Image = null;
+            }
+        }
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            if (pictureBox6.Image != null)
+            {
+                dispBoard(globalhand[5]);
+                pictureBox6.Image = null;
+            }
+        }
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+            if (pictureBox7.Image != null)
+            {
+                dispBoard(globalhand[6]);
+                pictureBox7.Image = null;
+            } 
+        }
+
+        private void boardPic1_Click(object sender, EventArgs e) 
+        {
+            if (boardPic1.Image != null)
+            {
+                dispHand(globalBoard[0]);
+                boardPic1.Image = null;
+                globalBoard[0] = (char)0;
+            } 
+        }
+
+        private void boardPic2_Click(object sender, EventArgs e)
+        {
+            if (boardPic2.Image != null)
+            {
+                dispHand(globalBoard[1]);
+                boardPic2.Image = null;
+                globalBoard[1] = (char)0;
+            }
+        }
+
+        private void boardPic3_Click(object sender, EventArgs e)
+        {
+            if (boardPic3.Image != null)
+            {
+                dispHand(globalBoard[2]);
+                boardPic3.Image = null;
+                globalBoard[2] = (char)0;
+            }
+        }
+
+        private void boardPic4_Click(object sender, EventArgs e)
+        {
+            if (boardPic4.Image != null)
+            {
+                dispHand(globalBoard[3]);
+                boardPic4.Image = null;
+                globalBoard[3] = (char)0;
+            }
+        }
+
+        private void boardPic5_Click(object sender, EventArgs e)
+        {
+            if (boardPic5.Image != null)
+            {
+                dispHand(globalBoard[4]);
+                boardPic5.Image = null;
+                globalBoard[4] = (char)0;
+            }
+        }
+
+        private void boardPic6_Click(object sender, EventArgs e)
+        {
+            if (boardPic6.Image != null)
+            {
+                dispHand(globalBoard[5]);
+                boardPic6.Image = null;
+                globalBoard[5] = (char)0;
+            }
+        }
+
+        private void boardPic7_Click(object sender, EventArgs e)
+        {
+            if (boardPic7.Image != null)
+            {
+                dispHand(globalBoard[6]);
+                boardPic7.Image = null;
+                globalBoard[6] = (char)0;
+            }
+        }
+        
+        private void button3_Click(object sender, EventArgs e)
+        {
+            String word;
+            word = "";
+
+            for (int i=0; i < 7; i++)
+            {
+                if (globalBoard[i] != (char)0) word += globalBoard[i];
+            }
+            load.loadWords(wordList);
+            bool isValid = load.isValidWord(word, null, wordList);
+
+            if (isValid)
+            {
+                
+                MessageBox.Show("Congratulations! You guessed word correctly!", "Success",
+                    MessageBoxButtons.OK);
+                clearBoard();
+                getPoints(word);
+            }
+            else
+            {
+                MessageBox.Show("Oops, there is a mistake! Check your spelling.", "Please try again.",
+                    MessageBoxButtons.OK);
+            }
+        }
+        
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            clearBoard();
+            
+            string hand = "";  
+            int n = 7;
+            MainClass play = new MainClass();
+            MainClass deal = new MainClass();
+            hand = deal.dealHand(n);
+            globalhand = hand.ToCharArray();
+            dispPics(hand.ToCharArray());
+            musicPlayer();
+           
+
+            
+        }
+
+
+
+        private void dispHand(char letter)
+        {
+            if (pictureBox1.Image == null)
+            {
+                String path = Environment.CurrentDirectory;
+                path = path + "\\Letters\\" + letter + ".png";
+                Image paveiksl = Image.FromFile(path);
+                pictureBox1.Image = paveiksl;
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+                globalhand[0] = letter;
+            }
+
+            else if (pictureBox2.Image == null)
+            {
+                String path = Environment.CurrentDirectory;
+                path = path + "\\Letters\\" + letter + ".png";
+                Image paveiksl = Image.FromFile(path);
+                pictureBox2.Image = paveiksl;
+                pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+                globalhand[1] = letter;
+            }
+
+            else if (pictureBox3.Image == null)
+            {
+                String path = Environment.CurrentDirectory;
+                path = path + "\\Letters\\" + letter + ".png";
+                Image paveiksl = Image.FromFile(path);
+                pictureBox3.Image = paveiksl;
+                pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+                globalhand[2] = letter;
+            }
+
+            else if (pictureBox4.Image == null)
+            {
+                String path = Environment.CurrentDirectory;
+                path = path + "\\Letters\\" + letter + ".png";
+                Image paveiksl = Image.FromFile(path);
+                pictureBox4.Image = paveiksl;
+                pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+                globalhand[3] = letter;
+            }
+
+            else if (pictureBox5.Image == null)
+            {
+                String path = Environment.CurrentDirectory;
+                path = path + "\\Letters\\" + letter + ".png";
+                Image paveiksl = Image.FromFile(path);
+                pictureBox5.Image = paveiksl;
+                pictureBox5.SizeMode = PictureBoxSizeMode.Zoom;
+                globalhand[4] = letter;
+            }
+
+            else if (pictureBox6.Image == null)
+            {
+                String path = Environment.CurrentDirectory;
+                path = path + "\\Letters\\" + letter + ".png";
+                Image paveiksl = Image.FromFile(path);
+                pictureBox6.Image = paveiksl;
+                pictureBox6.SizeMode = PictureBoxSizeMode.Zoom;
+                globalhand[5] = letter;
+            }
+
+            else if (pictureBox7.Image == null)
+            {
+                String path = Environment.CurrentDirectory;
+                path = path + "\\Letters\\" + letter + ".png";
+                Image paveiksl = Image.FromFile(path);
+                pictureBox7.Image = paveiksl;
+                pictureBox7.SizeMode = PictureBoxSizeMode.Zoom;
+                globalhand[6] = letter;
+            }
+
+        }
+
+
+        private void dispBoard(char letter)
+        {
+            if (boardPic1.Image == null)
+            {
+                String path = Environment.CurrentDirectory;
+                path = path + "\\Letters\\" + letter + ".png";
+                Image paveiksl = Image.FromFile(path);
+                boardPic1.Image = paveiksl;
+                boardPic1.SizeMode = PictureBoxSizeMode.Zoom;
+                globalBoard[0] = letter;
+            }
+            else if (boardPic2.Image == null)
+            {
+                String path = Environment.CurrentDirectory;
+                path = path + "\\Letters\\" + letter + ".png";
+                Image paveiksl = Image.FromFile(path);
+                boardPic2.Image = paveiksl;
+                boardPic2.SizeMode = PictureBoxSizeMode.Zoom;
+                globalBoard[1] = letter;
+            }
+            else if (boardPic3.Image == null)
+            {
+                String path = Environment.CurrentDirectory;
+                path = path + "\\Letters\\" + letter + ".png";
+                Image paveiksl = Image.FromFile(path);
+                boardPic3.Image = paveiksl;
+                boardPic3.SizeMode = PictureBoxSizeMode.Zoom;
+                globalBoard[2] = letter;
+            }
+            else if (boardPic4.Image == null)
+            {
+                String path = Environment.CurrentDirectory;
+                path = path + "\\Letters\\" + letter + ".png";
+                Image paveiksl = Image.FromFile(path);
+                boardPic4.Image = paveiksl;
+                boardPic4.SizeMode = PictureBoxSizeMode.Zoom;
+                globalBoard[3] = letter;
+            }
+            else if (boardPic5.Image == null)
+            {
+                String path = Environment.CurrentDirectory;
+                path = path + "\\Letters\\" + letter + ".png";
+                Image paveiksl = Image.FromFile(path);
+                boardPic5.Image = paveiksl;
+                boardPic5.SizeMode = PictureBoxSizeMode.Zoom;
+                globalBoard[4] = letter;
+            }
+            else if (boardPic6.Image == null)
+            {
+                String path = Environment.CurrentDirectory;
+                path = path + "\\Letters\\" + letter + ".png";
+                Image paveiksl = Image.FromFile(path);
+                boardPic6.Image = paveiksl;
+                boardPic6.SizeMode = PictureBoxSizeMode.Zoom;
+                globalBoard[5] = letter;
+            }
+            else if (boardPic7.Image == null)
+            {
+                String path = Environment.CurrentDirectory;
+                path = path + "\\Letters\\" + letter + ".png";
+                Image paveiksl = Image.FromFile(path);
+                boardPic7.Image = paveiksl;
+                boardPic7.SizeMode = PictureBoxSizeMode.Zoom;
+                globalBoard[6] = letter;
+            }
+        }
+
+
+
+        private void dispPics(Char[] hand)
+        {
+
+            String path = Environment.CurrentDirectory;
+            path = path + "\\Letters\\" + hand[0] + ".png";
+            Image paveiksl = Image.FromFile(path);
+            pictureBox1.Image = paveiksl;
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+
+            path = Environment.CurrentDirectory;
+            path = path + "\\Letters\\" + hand[1] + ".png";
+            paveiksl = Image.FromFile(path);
+            pictureBox2.Image = paveiksl;
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+
+            path = Environment.CurrentDirectory;
+            path = path + "\\Letters\\" + hand[2] + ".png";
+            paveiksl = Image.FromFile(path);
+            pictureBox3.Image = paveiksl;
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+
+            path = Environment.CurrentDirectory;
+            path = path + "\\Letters\\" + hand[3] + ".png";
+            paveiksl = Image.FromFile(path);
+            pictureBox4.Image = paveiksl;
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+
+            path = Environment.CurrentDirectory;
+            path = path + "\\Letters\\" + hand[4] + ".png";
+            paveiksl = Image.FromFile(path);
+            pictureBox5.Image = paveiksl;
+            pictureBox5.SizeMode = PictureBoxSizeMode.Zoom;
+
+            path = Environment.CurrentDirectory;
+            path = path + "\\Letters\\" + hand[5] + ".png";
+            paveiksl = Image.FromFile(path);
+            pictureBox6.Image = paveiksl;
+            pictureBox6.SizeMode = PictureBoxSizeMode.Zoom;
+
+            path = Environment.CurrentDirectory;
+            path = path + "\\Letters\\" + hand[6] + ".png";
+            paveiksl = Image.FromFile(path);
+            pictureBox7.Image = paveiksl;
+            pictureBox7.SizeMode = PictureBoxSizeMode.Zoom;
+
+        }
+
+        private void clearBoard()
+        {
+            boardPic1.Image = null;
+            boardPic2.Image = null;
+            boardPic3.Image = null;
+            boardPic4.Image = null;
+            boardPic5.Image = null;
+            boardPic6.Image = null;
+            boardPic7.Image = null;
+
+            for (int i = 0; i<7; i++)
+            {
+                globalBoard[i] = (char)0;
+            }
+        }
+
+        private void getPoints(string word)
+        {
+            int ans =+ load.getWordScore(word, 7);
+            pts = pts+ ans;
+            label1.Text = pts.ToString();
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+        
+        private void restart_Click(object sender, EventArgs e)
+        {
+            clearBoard();
+            string hand = "";
+            int n = 7;
+            MainClass play = new MainClass();
+            MainClass deal = new MainClass();
+            hand = deal.dealHand(n);
+            globalhand = hand.ToCharArray();
+            dispPics(hand.ToCharArray());
+        }
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox3_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+            
+        }
+            
+        public void musicPlayer()
+        {
+
+
+            String musicPath;
+            musicPath = Environment.CurrentDirectory + "\\music.wav";
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(musicPath);
+            player.PlayLooping();
+
+     
+            
+            
+        }
+
+        private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Globalization;
+using System.Runtime.InteropServices;
+
+
+namespace Informatikos_projektas
+{
+	class MainClass
+	{
+
+		public void loadWords (List <string> wordList)
+		{
+			Console.WriteLine("Loading word list from file...");
+			const string f = "words.txt";
+			try{
+				using (StreamReader sr = new StreamReader(f))
+				{
+					String line;
+				while ((line = sr.ReadLine ()) != null)
+				{
+					//line.ToLower();
+					wordList.Add (line.ToLower());
+				}
+				wordList = wordList.ConvertAll(low => low.ToLowerInvariant());
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("The file could not be read:");
+				Console.WriteLine(e.Message);
+			}
+		}
+		//------------------------------------------------//
+		public string dealHand(int n)
+		{
+			string Vowels = "aeiou";
+			char[] AllLetters = {'b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z','a','e','i','o','u'
+			};
+			char[] VowelsArray = Vowels.ToCharArray();
+			char [] handArray = new char[n]; 
+			string hand="";
+			Random numGenerator = new Random ();
+			for (int i = 0; i < 3; i++)
+				handArray [i] = Vowels [numGenerator.Next (0, 5)];
+			for (int j = 3; j < n; j++)
+				handArray [j] = AllLetters [numGenerator.Next (0, 26)];
+			hand =  new string(handArray);	
+			return hand;
+			}
+		//------------------------------------------------//
+		public void displayHand (string hand)
+		{
+			char[] sym = hand.ToCharArray();
+			for (int i = 0; i < sym.Length; i++) 
+			{
+				Console.Write (sym [i]);
+				Console.Write (" ");
+			}
+			Console.Write ("\n");
+		}
+		//------------------------------------------------//
+		public int getWordScore (string input, int n)
+		{
+			char[] sym = input.ToCharArray();
+				int ans = 0;
+			for (int i=0; i<input.Length; i++)
+			{
+				switch (sym[i]) {
+				case'a': ans += 1; break;
+				case'e': ans += 1; break;
+				case'i': ans += 1; break;
+				case'o': ans += 1; break;
+				case'r': ans += 1; break;
+				case's': ans += 1; break;
+				case't': ans += 1; break;
+				case'u': ans += 1; break;
+				case'n': ans += 1; break;
+				case'd': ans += 2; break;
+				case'g': ans += 2; break;
+				case'b': ans += 3; break;
+				case'c': ans += 3; break;
+				case'm': ans += 3; break;
+				case'p': ans += 3; break;
+				case'f': ans += 4; break;
+				case'h': ans += 4; break;
+				case'v': ans += 4; break;
+				case'w': ans += 4; break;
+				case'y': ans += 4; break;
+				case'k': ans += 5; break;
+                case'l': ans += 5; break;
+				case'j': ans += 8; break;
+				case'x': ans += 8; break;
+				case'q': ans += 10; break;
+				case'z': ans += 10; break;
+				}
+				}
+			if (input.Length == n)
+					ans += 50;
+				return ans;
+		}
+		//------------------------------------------------//
+		/*patikrina ar sarase ira ivestas zodis*/
+		public bool isValidWord (string input, string hand,  List <string> wordList)
+		{
+			bool check = false;
+			for (int i = 0; i < wordList.Count; i++)
+				if(wordList[i] == input){
+					check = true;
+				}
+			return check;
+		}
+		//------------------------------------------------//
+		/*Tikrina ar visos ivestos raides yra rankoj, galutinej versijoj tikriausiai nebebus eikalinga*/  /* Taip, del to ir nepridedu :) M. */
+		public bool genueLetters (string input, string hand)
+		{
+			bool check1;
+			for (int i = 0; i < input.Length; i++)
+			{
+				check1=false;
+				for (int j = 0; j < hand.Length; j++)
+				{
+					if (input [i] == hand [j]) {
+						check1 = true;
+						break;
+					}
+				}
+				if (check1 == false)
+						return false;
+			}
+			return true;
+		}
+		//------------------------------------------------//
+		/*Apskaiciuoja kiek rankoje yra raidziu*/
+		public int  calculateHandlen (string hand)
+		{
+			int len = 0;
+			for (int i = 0; i < hand.Length; i++) 
+			{
+				char[] sym = hand.ToCharArray ();
+				if (hand [i] == Convert.ToChar(" ")) // skaiciuoja kiek rankoj yra simboliu iki pirmo tarpo
+					return len;
+				else
+				len++;
+			}
+			return len;
+		}
+		//------------------------------------------------//
+		/*Pasalina panaudotas raides*/
+		public string deleteUsed (string hand, int n, string input)
+		{
+			char[] symh = hand.ToCharArray();
+			char[] symi = input.ToCharArray();
+			for (int i = 0; i < symi.Length; i++) 
+				for (int j = 0; j < symh.Length; j++)
+				{
+					if (symh [j] == symi [i]) {
+						for (int l = j; l < symh.Length - 1; l++)
+							symh [l] = symh [l + 1];                             //Is rankos pasalina panaudotas raides, perstumiant reiksmes su didesniu indeksu i ju vieta
+						symh [symh.Length - 1] = Convert.ToChar (" ");           //Paskutini simboli pakeicia tarpu, kad nebutu daug vienodu simboliu po perstumimo
+						break;
+					}    
+				}
+			hand =  new string(symh);
+			return hand;
+		}
+		//------------------------------------------------//
+		public void playHand (string hand, List <string> wordList, int n)
+		{
+			int score, sumScore=0;
+			string input, handnew=hand;
+			bool match;
+			Console.WriteLine ("your letters:");
+			displayHand (handnew);
+			while (calculateHandlen(hand)>= 0) //vygdo cikla kol rankoje yra raidziu
+				{
+					Start:
+					Console.WriteLine ("Enter word, or . to end the hand");
+					input = Convert.ToString (Console.ReadLine ());
+					if (input == ".")
+					break; // Jei ivedamas . iseina is ciklo
+				if (genueLetters (input, handnew) == false) 
+				{
+					Console.WriteLine ("You entered a letter witch is not in your hand, try again.");
+					goto Start;  //Jei ivedama neturima raide, gryzta i ciklo pradzia
+				}
+					match = isValidWord (input, handnew, wordList);
+					if (match == false)
+					{
+						Console.WriteLine("Ops, there is a mistake! Check your spelling.");
+						goto Start; // jei sarase nera ivesto zodzio grizta i ciklo pradzia
+					}
+					score = getWordScore (input, n);
+					Console.WriteLine("Score of word that you entered: "+score);
+					handnew = deleteUsed (handnew, n, input);
+				Console.WriteLine ("Remaining letters");
+				deleteUsed (handnew, n, input);
+				displayHand (handnew);
+					sumScore += score;
+				}
+			Console.WriteLine ("The end of this hand");
+			Console.WriteLine ("Your total score of this hand: " + sumScore + " points");
+		}
+		//------------------------------------------------//
+
+       
+		public static void Main (string[] args)
+			{
+
+            //```````````````````````````//
+         
+
+            Form1 Langas = new Form1();
+            Langas.ShowDialog();
+            //``````````````gui``````````````//
+           
+
+            /*
+			MainClass load = new MainClass ();
+			List<string> wordList = new List <string> ();
+			load.loadWords(wordList);
+			string hand = "";  //raides is kuriu deliojami zodziai (cia reikalingos tik patikrinimui)
+			int n = 7;
+			MainClass play = new MainClass ();
+			MainClass deal = new MainClass ();
+			string x = "n";
+			while (x != "e")
+			{
+				if (x == "n")
+				{
+					hand = deal.dealHand(n);
+					x = "r";
+				}
+				if (x == "r")
+				{
+					Console.WriteLine("Zaidimas pradedamas");
+					play.playHand(hand, wordList, n);
+				}
+				else 
+					Console.WriteLine("Your input was invalid");
+				Console.WriteLine("-------------------------------");
+				Console.WriteLine("---|Start new  game press n|---");
+				Console.WriteLine("-------------------------------");
+				Console.WriteLine("---|Start last game press r|---");
+				Console.WriteLine("-------------------------------");
+				Console.WriteLine("---|Exit  the  game press e|---");
+				Console.WriteLine("-------------------------------");
+				string a = Convert.ToString(Console.ReadLine());
+				x = a;
+             
+			}
+            */
+		}
+	}
+}
+
